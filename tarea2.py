ad7e79f6aa968c64aca11f506ff6d0944ba1ba6a
TDocmac <jtdocmac@uc.cl>
2016-08-21 14:21:04 -0400

    Update tarea2.py

--- tarea2.py
+++ tarea2.py
@@ -1,61 +1,85 @@
 import numpy as np
+from numpy import float32
+from scipy.sparse import dok_matrix
+import time
 
-'''def similitud(user,cercano):
-
-    usuario = open("ratings.dat")
-    lista_user = []
-    lista_cercano = []
-    for linea in usuario:
-        if str(user) == linea.strip().split("::")[0]:
-            lista_user.append(linea.strip().split("::")[1])
-        elif str(cercano) == linea.strip().split("::")[0]:
-            lista_cercano.append(linea.strip().split("::")[1])
-    conjunto_user = set(lista_user)
-    conjunto_cercano = set(lista_cercano)
-    interseccion = conjunto_user & conjunto_cercano
-    usuario.close()
-    '''
-
-'''archivo1 = np.loadtxt("ratings.dat", dtype = "float" ,comments = "#", delimiter = "::", usecols = (0,1,2))
-np.save("archivo2",archivo1)'''
-
-
-archivo = np.load("archivo2.npy")
-'''
-final = []
-i = 0
-for linea in archivo:
-    if i < linea[0]:
-        i += 1
-        final.append([i,[]])
-    if i == linea[0]:
-        final[i-1][1].append((linea[1],linea[2]))
-'''
-
-
-final_peli = {}
-for linea in archivo:
-    if linea[1] not in final_peli:
-        final_peli[linea[1]] = []
-    final_peli[linea[1]].append((linea[0],linea[2]))
-
-
+t0 = time.time()
 
 ratings = open("ratings.dat")
 
-d={}
+dict_movies = {}
+dict_user = {}
+matriz = dok_matrix((71568, 65134),dtype=float32)
+
+
 for linea in ratings:
     linear = linea.strip().split("::")
-    d[int(linear[0]),int(linear[1])]=(linear[2])
+    linear = map(float,linear)
+    if linear[1] not in dict_movies:
+        dict_movies[linear[1]] = []
+    if linear[0] not in dict_user:
+        dict_user[linear[0]] = []
+    dict_movies[linear[1]].append(linear[0])
+    dict_user[linear[0]].append(linear[1])
+    matriz[linear[0], linear[1]] = linear[2]
+
+ratings.close()
+
+
+t1 = time.time()
+
+
+def similitud(user1,user2,matrix):
+    lista1 = []
+    lista2 = []
+    lista_rat = []
+    conj = set(dict_user[user1]) & set(dict_user[user2])
+    for j in conj:
+            lista_rat.append(matrix[user1,j] * matrix[user2,j])
+            lista1.append(matrix[user1,j]**2)
+            lista2.append(matrix[user2,j]**2)
+    suma1 = sum(lista1)
+    suma2 =  sum(lista2)
+    suma_ratings = sum(lista_rat)
+    if suma1 == 0  or suma2 == 0:
+        return 0
+    divisor = (suma1**0.5) * (suma2**0.5)
+
+    return suma_ratings/divisor
+
+t2 = time.time()
+
+def prediccion(columna,user,matrix):
+    lista_suma = []
+    lista_div = []
+    for j in dict_movies[columna]:
+            sim = similitud(user, j, matrix)
+            lista_suma.append(matriz[j, columna] * sim)
+            lista_div.append(sim)
+    suma = sum(lista_suma)
+    divisor = sum(lista_div)
+    if divisor == 0:
+        return 0
+    return suma/divisor
+
+
+
+
+#print similitud(1,3,matriz)
+print prediccion(518,1,matriz)
+t3 = time.time()
+print t3-t1
 
 def top10(usuario):
     a =[0,0,0,0,0,0,0,0,0,0]
     for movie in dict_movies:
-        p = prediccion(usuario, movie)
+        p = prediccion(usuario, movie,matriz)
         for r in range(len(a)):
             if p>a[r]:
                 a[r]=p
                 break
     return a
+
+#print top10(1)
 
 
